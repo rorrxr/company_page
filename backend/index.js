e20b89873a5d1642a53f00b6c343fc5dd4f4f85d
muchluvu <rorrxr@naver.com>
2025-09-16 12:00:00 +0900

    [chore] : 백엔드 프로젝트 초기 설정

---
+++ backend/index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const app = express();
+const PORT = 3000;
+
+const userRoutes = require("./routes/user");
+const contactRoutes = require("./routes/contact");
+const postRoutes = require("./routes/post");
+const uploadRoutes = require("./routes/upload");
+
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
+
+app.use(express.json())
+app.use(express.urlencoded())
+app.use(cookieParser());
+
+app.use("/api/auth", userRoutes);
+app.use("/api/contact", contactRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/upload", uploadRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB와 연결이 되었습니다."))
+  .catch((error) => console.log("MongoDB와 연결에 실패했습니다: ", error));
+
+app.listen(PORT, () => {
+  console.log("Server is running");
+});
