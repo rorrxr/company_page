cef4e273f79d1b1749452634d74ccba3cef8a7fc
muchluvu <rorrxr@naver.com>
2025-09-18 12:00:00 +0900

    [feat] : Contact Routes의 문의글 상태 변경 및 삭제 구현

--- backend/routes/ContactRoutes.js
+++ backend/routes/ContactRoutes.js
@@ -67,4 +67,37 @@ router.get('/:id', authenticateToken, async (req, res) => {
     }
 });
 
+// 문의 상태 업데이트
+router.put('/:id', authenticateToken, async (req, res) => {
+    try{
+        const { status } = req.body;
+
+        const contact = await Contact.findByIdAndUpdate(req.params.id);
+        // 문의가 없을 때
+        if(!contact){
+            return res.status(404).json({ message: '문의를 찾을 수 없습니다' });
+        }
+
+        res.status(200).json({ message: '문의가 성공적으로 업데이트되었습니다.' });
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ message: '서버 오류' });
+    }
+});
+
+// 문의 삭제
+router.delete('/:id', authenticateToken, async (req, res) => {
+    try{
+        const contact = await Contact.findByIdAndDelete(req.params.id);
+        // 문의가 없을 때
+        if(!contact){
+            return res.status(404).json({ message: '문의를 찾을 수 없습니다' });
+        }
+        res.status(200).json({ message: '문의가 성공적으로 삭제되었습니다.' });
+    }catch(err){
+        console.error(err);
+        res.status(500).json({ message: '서버 오류' });
+    }
+});
+
 module.exports = router;
